ca2b4acf26258c18d1f061c20103d7432aaa7ae3
Jean Joeris <joeris.peter@gmail.com>
2016-08-04 08:57:25 -0600

    updated and refactored lost_revenue_experiment

--- lost_revenue_experiment.rb
+++ lost_revenue_experiment.rb
@@ -11,22 +11,65 @@ se = SalesEngine.from_csv({
   :transactions => './data/transactions.csv'
   })
 
-sa = SalesAnalyst.new(se)
-num_merchants = se.all_merchants.count
-num_merchants_with_returns = sa.merchants_with_returned_invoices.count
-percent_merchants_with_returns = 100 * (num_merchants_with_returns / num_merchants.to_f)
-lost_revenue_percentages = sa.lost_revenue_percentages
-worst_revenue_ratio = lost_revenue_percentages.values.max
-num_merchants_with_actual_returns = lost_revenue_percentages.reduce(0) do |result, revenue|
-  result += 1 if revenue[1] != 0
-  result
-end
-puts lost_revenue_percentages
-puts "Out of #{num_merchants} merchants, #{num_merchants_with_returns}"\
-    " merchants had returns. These merchants accounted for "\
-    "#{percent_merchants_with_returns.round(2)}%"\
-    "\nHowever, only #{num_merchants_with_actual_returns} had returns "\
-    "on succesful transactions"
-puts "\nThe worst merchant for customer satisfaction is: \n"\
-    "Merchant ID - #{lost_revenue_percentages.key(worst_revenue_ratio)} "\
-    "with a ratio of #{worst_revenue_ratio}% returns"
+  sa = SalesAnalyst.new(se)
+
+  class LostRevenueLogic
+
+    def initialize(sales_analyst, sales_engine)
+      @sa = sales_analyst
+      @se = sales_engine
+      @lost_revenue_percentages = @sa.lost_revenue_percentages
+      @worst_revenue_ratio = @lost_revenue_percentages.values.max
+    end
+
+    def num_merchants
+      num_merchants = @se.all_merchants.count
+    end
+
+    def num_merchants_with_returns
+      num_merchants_with_returns = @sa.merchants_with_returned_invoices.count
+    end
+
+    def worst_merchants
+      @lost_revenue_percentages.collect do |merchant_id, revenue_ratio|
+        @se.find_merchant_by_merchant_id(merchant_id) if revenue_ratio == @worst_revenue_ratio
+      end.compact
+    end
+
+    def num_merchants_with_actual_returns
+      @lost_revenue_percentages.reduce(0) do |result, revenue|
+        result += 1 if revenue[1] != 0
+        result
+      end
+    end
+
+    def percent_merchants_with_returns
+      100 * (num_merchants_with_returns / num_merchants.to_f)
+    end
+
+    def percent_merchants_with_actual_returns
+      100 * (num_merchants_with_actual_returns / num_merchants.to_f)
+    end
+
+
+    def shaming_string(merchant)
+      "#{merchant.name} lost #{@worst_revenue_ratio}% of "\
+      "their #{merchant.paid_invoices.count} succesful invoices"
+    end
+
+    def returned_metrics_string
+      "\nOut of #{num_merchants} merchants, #{num_merchants_with_returns}"\
+      " merchants had returns. These merchants accounted for "\
+      "#{percent_merchants_with_returns.round(2)}%"\
+      "\nHowever, only #{num_merchants_with_actual_returns} had returns "\
+      "on succesful transactions. These merchants accounted for "\
+      "#{percent_merchants_with_actual_returns.round(2)}%"
+    end
+  end
+  lrl = LostRevenueLogic.new(sa, se)
+
+  puts lrl.returned_metrics_string
+  puts "\nThe worst merchants for customer returns are: \n"
+  lrl.worst_merchants.each do |merchant|
+    puts lrl.shaming_string(merchant)
+  end
